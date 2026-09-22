b8470748bf524b8edaef206df24d4cf624676eba
gdjourney0410-del <gd.journey0410@gmail.com>
2026-09-22 10:58:53 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE='kasrumah-v1';
+const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',e=>{
+  const req=e.request;
+  if(req.method!=='GET')return;
+  e.respondWith(caches.match(req,{ignoreSearch:true}).then(hit=>{
+    const net=fetch(req).then(res=>{
+      if(res&&(res.ok||res.type==='opaque')){const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy));}
+      return res;
+    }).catch(()=>{
+      if(hit)return hit;
+      if(req.mode==='navigate')return caches.match('./index.html');
+      return Response.error();
+    });
+    return hit||net;
+  }));
+});
